73e48590db51c732b892518201ab5ccda921e09a
teethfish <teethfish@hotmail.com>
2015-11-10 10:20:15 -0500

    Update calculate_domain_info.m

--- calculate_domain_info.m
+++ calculate_domain_info.m
@@ -22,3 +22,28 @@ Lz = Ze - Zs;
 dz = Lz/Zn;
 
 end
+
+function [Xs, Xe, Lx, dx, Nx, Ys, Ye, Ly, dy, Ny, Zs, Ze, Lz, dz, Nz] = calculate_domain_info(casename)
+
+[X, Y, Z] = cgns_read_grid(casename);
+[Xn, Yn, Zn] = size(X);
+Xn = Xn - 1;
+Yn = Yn - 1;
+Zn = Zn - 1;
+
+Xs = X(1,1,1);
+Xe = X(end,1,1);
+Lx = Xe - Xs;
+dx = Lx/Xn;
+
+Ys = Y(1,1,1);
+Ye = Y(1,end,1);
+Ly = Ye - Ys;
+dy = Ly/Yn;
+
+Zs = Z(1,1,1);
+Ze = Z(1,1,end);
+Lz = Ze - Zs;
+dz = Lz/Zn;
+
+end
